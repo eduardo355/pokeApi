a2e21ad6d878135e60e564f5570bca161eb85762
couch355 <tovareduardo75@gmail.com>
2024-03-16 01:09:11 -0600

    Aded new icon in tittle

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,10 +12,13 @@ const NavBar = () => {
     const handleResetear = () => {
         busquedaReset()
     }
-
+    const urlImg = './pokedex.png'
     return (
         <div className=" flex p-4 items-center justify-between bg-red-500 max-sm:w-screen ">
-            <h1 className="text-4xl font-bold text-white max-sm:text-2xl">POKEDEX</h1>
+            <div className="flex items-center gap-4">
+                <img src={urlImg} alt="Pokedex" />
+                <h1 className="text-4xl font-bold text-white max-sm:text-2xl max-sm:hidden">POKEDEX</h1>
+            </div>
             <div className=" flex gap-4">
                 <input 
                     className=" p-1 w-80 max-sm:w-40 focus:outline-none bg-transparent text-white border-b-2" 
